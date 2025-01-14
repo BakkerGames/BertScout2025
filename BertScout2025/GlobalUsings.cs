fc6608060c6c6b8e208aaa7dc0694b1e1012b6bb
Scott Bakker <sbakker343@gmail.com>
2025-01-14 18:50:02 -0500

    Revert "Add project files."
    This reverts commit 80d869cb95278377f91d73f310fe884165a44df2.

--- BertScout2025/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using BertScout2025.Data;
-global using BertScout2025.PageModels;
-global using BertScout2025.Pages;
-global using BertScout2025.Services;
-global using BertScout2025.Utilities;
-global using Fonts;
--- BertScout2025/Models/ProjectsTags.cs
+++
@@ -1,9 +0,0 @@
-namespace BertScout2025.Models
-{
-    public class ProjectsTags
-    {
-        public int ID { get; set; }
-        public int ProjectID { get; set; }
-        public int TagID { get; set; }
-    }
-}
